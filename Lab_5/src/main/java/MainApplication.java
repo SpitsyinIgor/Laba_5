d6d3a3f74dfa4b1cef32fe72bd6761cf80d86709
SpitsyinIgor <1overflash1@gmail.com>
2024-12-20 10:38:37 +0300

    Update MainApplication.java

--- Lab_5/src/main/java/MainApplication.java
+++ Lab_5/src/main/java/MainApplication.java
@@ -19,3 +19,20 @@ class SecondImplementation implements FirstInterface {
 class SecondExecutor implements SecondInterface {
     public void perform() { System.out.println("Z"); }
 }
+
+class Bean {
+    @AutoInjectable private FirstInterface firstField;
+    @AutoInjectable private SecondInterface secondField;
+
+    public void run() {
+        firstField.execute();
+        secondField.perform();
+    }
+}
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Bean beanInstance = (new DependencyInjector()).injectDependencies(new Bean());
+        beanInstance.run();
+    }
+}
